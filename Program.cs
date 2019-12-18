727ec047f8e5ad8d89d10c2e0e6e501d5aa94385
Stephen Hlotyak <hlotyaks@ter.teradyne.com>
2019-12-18 12:04:04 -0500

    scafolding for new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecursiveHasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
