18e20321e17df90b9aeaff221dcc53d12aa0d54e
Stephen Hlotyak <hlotyaks@ter.teradyne.com>
2019-12-20 09:19:02 -0500

    Hash woring on test folder

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
+using System.Net;
 
 namespace RecursiveHasher
 {
@@ -11,21 +13,32 @@ namespace RecursiveHasher
         {
             Program p = new Program();
 
-            Console.WriteLine(p.Run());
+            var folder = new DirectoryInfo(@"C:\tmp\experiment2\packageA");
+            var task = Task.Run(async () => await p.HashFolder(folder));
+            var result = task.Result;
 
-            //var task = Task
+            Console.WriteLine($"Hash of {folder.Name} in {folder.FullName}");
+            Console.WriteLine(result);
+
         }
 
-        string Run()
-        {
-            return "Hello World";
-        }
-
-        async Task<byte[]> HashFolder(DirectoryInfo folder, string searchPattern = "*", SearchOption searchOption = SearchOption.TopDirectoryOnly)
+        async Task<string> HashFolder(DirectoryInfo folder, string searchPattern = "*", SearchOption searchOption = SearchOption.AllDirectories)
         {
             using(var alg = MD5.Create())
             {
-                return await alg.ComputeHashAsync(folder.EnumerateFiles(searchPattern, searchOption));
+                var result = await alg.ComputeHashAsync(folder.EnumerateFiles(searchPattern, searchOption));
+
+                // Build the final string by converting each byte
+                // into hex and appending it to a StringBuilder
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < result.Length; i++)
+                {
+                    sb.Append(result[i].ToString("X2"));
+                }
+
+                // And return it
+                return sb.ToString();
+
             }
         }
     }
